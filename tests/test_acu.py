c1700f49b39a5789d2cea8b7f19729273ce49ebf
Corpse89Grinder <giuseppecarboni89@live.com>
2017-08-28 18:28:10 +0200

    Fix #64 (#65)
    * Fix #64
    
    * Improved tests for ACU to increase coverage again.
    
    * Removed an unnecessary comma ','.

--- tests/test_acu.py
+++ tests/test_acu.py
@@ -25,37 +25,27 @@ class TestACU(unittest.TestCase):
 
     def test_status_message_length(self):
         status = self.system.get_status()
-        msg_length = (
-            ord(status[4]) * 0x1000000
-            + ord(status[5]) * 0x10000
-            + ord(status[6]) * 0x100
-            + ord(status[7])
-        )
+        msg_length = utils.bytes_to_int(status[4:8])
         self.assertEqual(msg_length, 539)
 
     def test_status_message_counter(self):
         for i in xrange(10):
             status = self.system.get_status()
-            msg_counter = (
-                ord(status[8]) * 0x1000000
-                + ord(status[9]) * 0x10000
-                + ord(status[10]) * 0x100
-                + ord(status[11])
-            )
+            msg_counter = utils.bytes_to_int(status[8:12])
             self.assertEqual(msg_counter, i)
 
-    def test_parse(self):
-        msg_length = bin(46)[2:].zfill(32)
-        cmd_counter = bin(utils.day_milliseconds())[2:].zfill(32)
-        cmds_counter = bin(1)[2:].zfill(32)
+    def test_parse_correct_end_flag(self):
+        msg_length = utils.int_to_twos(46)
+        cmd_counter = utils.int_to_twos(utils.day_milliseconds())
+        cmds_counter = utils.int_to_twos(1)
 
         # Command
-        cmd_id = bin(1)[2:].zfill(16)
-        sub_id = bin(1)[2:].zfill(16)
-        counter = bin(utils.day_milliseconds())[2:].zfill(32)
-        mode_id = bin(1)[2:].zfill(16)
-        par_1 = bin(0)[2:].zfill(64)
-        par_2 = bin(0)[2:].zfill(64)
+        cmd_id = utils.int_to_twos(1, 2)
+        sub_id = utils.int_to_twos(1, 2)
+        counter = utils.int_to_twos(utils.day_milliseconds())
+        mode_id = utils.int_to_twos(1, 2)
+        par_1 = utils.int_to_twos(0, 8)
+        par_2 = utils.int_to_twos(0, 8)
 
         command = (
             cmd_id
@@ -77,8 +67,7 @@ class TestACU(unittest.TestCase):
 
         msg = self.start_flag
 
-        for i in range(0, len(binary_msg), 8):
-            msg += chr(int(binary_msg[i:i + 8], 2))
+        msg += utils.binary_to_bytes(binary_msg)
 
         msg += self.end_flag
 
@@ -90,6 +79,51 @@ class TestACU(unittest.TestCase):
             [hex(ord(x)) for x in msg]
         )
 
+    def test_parse_wrong_end_flag(self):
+        msg_length = utils.int_to_twos(46)
+        cmd_counter = utils.int_to_twos(utils.day_milliseconds())
+        cmds_counter = utils.int_to_twos(1)
+
+        # Command
+        cmd_id = utils.int_to_twos(1, 2)
+        sub_id = utils.int_to_twos(1, 2)
+        counter = utils.int_to_twos(utils.day_milliseconds())
+        mode_id = utils.int_to_twos(1, 2)
+        par_1 = utils.int_to_twos(0, 8)
+        par_2 = utils.int_to_twos(0, 8)
+
+        command = (
+            cmd_id
+            + sub_id
+            + counter
+            + mode_id
+            + par_1
+            + par_2
+        )
+
+        commands = command  # Could be more than one command
+
+        binary_msg = (
+            msg_length
+            + cmd_counter
+            + cmds_counter
+            + commands
+        )
+
+        msg = self.start_flag
+
+        msg += utils.binary_to_bytes(binary_msg)
+
+        msg += self.end_flag
+
+        for byte in msg[:-1]:
+            self.assertTrue(self.system.parse(byte))
+
+        with self.assertRaises(ValueError):
+            self.system.parse('\x00')  # Wrong ending byte
+
+    def test_parse_wrong_start_flag(self):
+        self.assertFalse(self.system.parse('\x00'))
 
 if __name__ == '__main__':
     unittest.main()
