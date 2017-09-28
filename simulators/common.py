efbc1dd183e2c6faefc044cf8f7f2a181a546af7
Marco Buttu <marco.buttu@gmail.com>
2017-09-28 09:07:41 +0200

    Fix #85: make BaseSystem an ABC (#86)

--- simulators/common.py
+++ simulators/common.py
@@ -1,8 +1,23 @@
 import os
+import abc
 import signal
 
 
 class BaseSystem(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def parse(self, byte):
+        """This method takes a byte (single character string) and returns:
+        False when the given byte is not the header, but the header is
+        expected, True when the given byte is the header or a following
+        expected byte, the response (the string to be sent back to the client)
+        when the message is completed.
+        The method eventually raises a ValueError in one of the following
+        cases: the declared length of the message exceeds the maximum expected
+        length, the sent message carries a wrong checksum, the client asks to
+        execute an unknown command."""
 
     @staticmethod
     def system_stop():
