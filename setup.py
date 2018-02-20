0b954fc9cc6e456d40aa3efc4ade843aedcd9379
Giuseppe Carboni <giuseppecarboni89@live.com>
2018-02-20 12:55:22 +0100

    Fix #118, fixed travis testing environment.

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+additional_packages = [
+    'numpy',
+    'scipy',
+]
+
+extras = {
+    'test': additional_packages,
+}
+
 setup(
     name='discos-simulators',
     version='0.1',
@@ -15,5 +24,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
     ],
-    install_requires=['numpy', 'scipy'],
+    install_requires=additional_packages,
+    tests_require=additional_packages,
+    extras_require=extras,
 )
