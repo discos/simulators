0dc0fd82eab52bfd4d184a7e0143676040cd2a92
Giuseppe Carboni <giuseppecarboni89@live.com>
2019-06-13 14:06:16 +0200

    Added the weather station simulator
    Also, some minor fixes to coverage reporting

---
+++ simulators/weather_station/__init__.py
@@ -0,0 +1,136 @@
+import datetime
+from SocketServer import ThreadingUDPServer
+from threading import current_thread
+from simulators.common import ListeningSystem
+
+
+# Each system module (like active_surface.py, acu.py, etc.) has to
+# define a list called servers. This list contains tuples
+# (l_address, s_address, args). l_address is the tuple (ip, port) that
+# defines the listening node that exposes the parse method, s_address
+# is the tuple that defines the optional sending node that exposes the
+# subscribe and unsibscribe methods, while args is a tuple of optional
+# extra arguments.
+servers = [(('0.0.0.0', 12600), (), ThreadingUDPServer, ())]
+
+
+class System(ListeningSystem):
+
+    def __init__(self):
+        self.starting_date = \
+            datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+
+        self.commands = {
+            'r': '_read',
+            'w': '_write'
+        }
+
+        self.tail = '\n'
+
+        self.sensors = {
+            'th01': [26.0, self.starting_date, 'heating temp [oC]'],
+            'vh01': [0.0, self.starting_date, 'heating voltage [V]'],
+            'vs01': [7.7, self.starting_date, 'supply voltage [V]'],
+            'vr01': [3.52, self.starting_date, 'rif. voltage [V]'],
+            'dn01': [287.0, self.starting_date, 'wind dir min [deg]'],
+            'dm01': [289.0, self.starting_date, 'wind dir ave [deg]'],
+            'dx01': [292.0, self.starting_date, 'wind dir max [deg]'],
+            'sn01': [1.6, self.starting_date, 'wind speed min [m/s]'],
+            'sm01': [1.8, self.starting_date, 'wind speed ave [m/s]'],
+            'sx01': [1.9, self.starting_date, 'wind speed max [m/s]'],
+            'ta01': [23.0, self.starting_date, 'air temp [oC]'],
+            'ua01': [72.5, self.starting_date, 'rel. humidity [%]'],
+            'pa01': [940.9, self.starting_date, 'air pressure [hPa]'],
+            'rc01': [9.86, self.starting_date, 'rain amount [mm]'],
+            'rd01': [4240.0, self.starting_date, 'rain duration [s]'],
+            'ri01': [0.0, self.starting_date, 'rain intensity [mm/h]'],
+            'rp01': [-9999.9, '#', 'rain peak duration [s]'],
+            'hc01': [0.0, self.starting_date, 'hail amount [hits/cm2]'],
+            'hd01': [20.0, self.starting_date, 'hail duration [s]'],
+            'hi01': [0.0, self.starting_date, 'hail intensity [hits/cm2h]'],
+            'hp01': [-9999.9, '#', 'hail peak duration [s]']
+        }
+
+        self.err_string = (
+            '<Sensor>'
+            + '<Id>sintax error or sensor not found</Id>'
+            + '<Val>1.000000</Val>'
+            + '<Date>error</Date>'
+            + '<Info>error</Info>'
+            + '</Sensor>'
+        )
+
+        self.msg = {}
+
+    def _reset(self, t):
+        del self.msg[t]
+
+    def parse(self, byte):
+        t = current_thread().ident
+        if not self.msg.get(t):
+            self.msg[t] = b''
+
+        self.msg[t] += byte
+
+        if len(self.msg[t]) == 1:
+            if byte in self.commands.keys():
+                return True
+            else:
+                self._reset(t)
+                return False
+        elif len(self.msg[t]) == 2:
+            if byte == ' ':
+                return True
+            else:
+                self._reset(t)
+                return False
+        elif byte == self.tail:
+            self.msg[t] = self.msg[t].strip()
+            args = self.msg[t].split()
+
+            if len(args) > 1:
+                command = getattr(self, self.commands.get(args[0]))
+                sensor = args[1]
+            if len(args) == 2:
+                self._reset(t)
+                if command != self._read:
+                    return False
+                return self._read(sensor)
+            elif len(args) == 4:
+                if command != self._write:
+                    self._reset(t)
+                    return False
+                try:
+                    value = float(args[2])
+                except ValueError:
+                    value = 0.0
+                date = args[3]
+                self._reset(t)
+                return self._write(sensor, value, date)
+            else:
+                self._reset(t)
+                return False
+        return True
+
+    def _read(self, sensor):
+        try:
+            value, date, info = self.sensors[sensor]
+            retval = (
+                '<Sensor>'
+                + '<Id>%s</Id>' % sensor
+                + '<Val>%.6f</Val>' % float(value)
+                + '<Date>%s</Date>' % date
+                + '<Info>%s</Info>' % info
+                + '</Sensor>'
+            )
+        except KeyError:
+            retval = self.err_string
+        return retval
+
+    def _write(self, sensor, value, date):
+        try:
+            self.sensors[sensor][0] = value
+            self.sensors[sensor][1] = date
+        except KeyError:
+            pass
+        return self._read(sensor)
---
+++ tests/test_weather_station.py
@@ -0,0 +1,190 @@
+import unittest
+import datetime
+from xml.etree.ElementTree import fromstring
+from simulators import weather_station
+
+
+class TestWeatherStation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sensors = {
+            'th01': [26.0, 'heating temp [oC]'],
+            'vh01': [0.0, 'heating voltage [V]'],
+            'vs01': [7.7, 'supply voltage [V]'],
+            'vr01': [3.52, 'rif. voltage [V]'],
+            'dn01': [287.0, 'wind dir min [deg]'],
+            'dm01': [289.0, 'wind dir ave [deg]'],
+            'dx01': [292.0, 'wind dir max [deg]'],
+            'sn01': [1.6, 'wind speed min [m/s]'],
+            'sm01': [1.8, 'wind speed ave [m/s]'],
+            'sx01': [1.9, 'wind speed max [m/s]'],
+            'ta01': [23.0, 'air temp [oC]'],
+            'ua01': [72.5, 'rel. humidity [%]'],
+            'pa01': [940.9, 'air pressure [hPa]'],
+            'rc01': [9.86, 'rain amount [mm]'],
+            'rd01': [4240.0, 'rain duration [s]'],
+            'ri01': [0.0, 'rain intensity [mm/h]'],
+            'rp01': [-9999.9, 'rain peak duration [s]'],
+            'hc01': [0.0, 'hail amount [hits/cm2]'],
+            'hd01': [20.0, 'hail duration [s]'],
+            'hi01': [0.0, 'hail intensity [hits/cm2h]'],
+            'hp01': [-9999.9, 'hail peak duration [s]']
+        }
+
+        cls.err_string = (
+            '<Sensor>'
+            + '<Id>sintax error or sensor not found</Id>'
+            + '<Val>1.000000</Val>'
+            + '<Date>error</Date>'
+            + '<Info>error</Info>'
+            + '</Sensor>'
+        )
+
+    def setUp(self):
+        self.system = weather_station.System()
+
+    @staticmethod
+    def _expected(
+            sensor=None, val=None, date=None, info=None):  # pragma: no cover
+        retval = {}
+        if sensor:
+            retval['Id'] = sensor
+        if val:
+            retval['Val'] = val
+        if date:
+            retval['Date'] = date
+        if info:
+            retval['Info'] = info
+        return retval
+
+    @staticmethod
+    def _parseString(answer):  # pragma: no cover
+        retval = {}
+        for element in list(fromstring(answer)):
+            retval[element.tag] = element.text
+            if element.tag == 'Val':
+                try:
+                    retval[element.tag] = float(element.text)
+                except ValueError:
+                    pass
+        return retval
+
+    @staticmethod
+    def _compare(expected, response):  # pragma: no cover
+        for key in expected.keys():
+            try:
+                if not expected[key] == response[key]:
+                    return False
+            except KeyError:
+                return False
+        return True
+
+    def test_wrong_starting_byte(self):
+        self.assertFalse(self.system.parse('c'))
+
+    def test_no_space_after_command(self):
+        self.assertTrue(self.system.parse('r'))
+        self.assertFalse(self.system.parse('c'))  # Should be ` `
+
+    def test_read(self):
+        for sensor, values in self.sensors.iteritems():
+            cmd = 'r %s' % sensor
+            for byte in cmd:
+                self.assertTrue(self.system.parse(byte))
+            response = self._parseString(self.system.parse('\n'))
+            expected = self._expected(
+                sensor=sensor,
+                val=values[0],
+                info=values[1]
+            )
+            self.assertTrue(self._compare(expected, response))
+
+    def test_read_error(self):
+        cmd = 'r unkn'
+        for byte in cmd:
+            self.assertTrue(self.system.parse(byte))
+        response = self.system.parse('\n')
+        self.assertEqual(response, self.err_string)
+
+    def test_read_as_write(self):
+        date = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        args = (self.sensors.keys()[0], 10.0, date)
+        cmd = 'r %s %s %s' % args
+        for byte in cmd:
+            self.assertTrue(self.system.parse(byte))
+        self.assertFalse(self.system.parse('\n'))
+
+    def test_write(self):
+        date = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        for sensor, values in self.sensors.iteritems():
+            args = (sensor, 10.0, date)
+            cmd = 'w %s %s %s' % args
+            for byte in cmd:
+                self.assertTrue(self.system.parse(byte))
+            response = self._parseString(self.system.parse('\n'))
+            expected = self._expected(
+                sensor=sensor,
+                val=10.0,
+                date=date,
+                info=values[1]
+            )
+            self.assertTrue(self._compare(expected, response))
+
+    def test_write_wrong_value(self):
+        date = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        for sensor, values in self.sensors.iteritems():
+            args = (sensor, 'wrong', date)
+            cmd = 'w %s %s %s' % args
+            for byte in cmd:
+                self.assertTrue(self.system.parse(byte))
+            response = self._parseString(self.system.parse('\n'))
+            expected = self._expected(
+                sensor=sensor,
+                val=0.0,
+                date=date,
+                info=values[1]
+            )
+            self.assertTrue(self._compare(expected, response))
+
+    def test_write_wrong_date(self):
+        for sensor, values in self.sensors.iteritems():
+            args = (sensor, 10.0, 'wrong_date')
+            cmd = 'w %s %s %s' % args
+            for byte in cmd:
+                self.assertTrue(self.system.parse(byte))
+            response = self._parseString(self.system.parse('\n'))
+            expected = self._expected(
+                sensor=sensor,
+                val=10.0,
+                date='wrong_date',
+                info=values[1]
+            )
+            self.assertTrue(self._compare(expected, response))
+
+    def test_write_unknown_sensor(self):
+        date = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        args = ('unkn', 10.0, date)
+        cmd = 'w %s %s %s' % args
+        for byte in cmd:
+            self.assertTrue(self.system.parse(byte))
+        response = self.system.parse('\n')
+        self.assertEqual(response, self.err_string)
+
+    def test_write_wrong_number_of_args(self):
+        for sensor, _ in self.sensors.iteritems():
+            args = (sensor, 10.0)
+            cmd = 'w %s %s' % args
+            for byte in cmd:
+                self.assertTrue(self.system.parse(byte))
+            self.assertFalse(self.system.parse('\n'))
+
+    def test_write_as_read(self):
+        cmd = 'w %s' % self.sensors.keys()[0]
+        for byte in cmd:
+            self.assertTrue(self.system.parse(byte))
+        self.assertFalse(self.system.parse('\n'))
+
+
+if __name__ == '__main__':
+    unittest.main()
