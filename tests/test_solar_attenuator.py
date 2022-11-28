90304d6abe58d75773e91fa5fb2731a736283a9e
LorenzoMonti <lorenzomonti42@gmail.com>
2022-11-28 16:09:13 +0100

    #fix-issue-286: add solar attenuator simulator and its tests (#287)
    * add solar attenuator simulator and its tests
    
    * linter warning fixed
    
    * linter warning improvement
    
    * linter warning improvement
    
    * add test to improve the coverage
    
    * tests improved

---
+++ tests/test_solar_attenuator.py
@@ -0,0 +1,57 @@
+import unittest
+from simulators.solar_attenuator import System
+
+
+class TestSolarAttenuator(unittest.TestCase):
+
+    def setUp(self):
+        self.system = System()
+
+    def tearDown(self):
+        del self.system
+
+    def test_set_w_solar_attn(self, mode="attenuator"):
+        msg = 'set W_solar_attn\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, mode)
+
+    def test_set_w_cal(self, mode="calibrator"):
+        msg = 'set W_cal\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, mode)
+
+    def test_set_w_passthrough(self, mode="pass-through"):
+        msg = 'set W_passthrough\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, mode)
+
+    def test_get_w_cal_temp(self, cal_temp=21.0):
+        msg = 'get W_cal_temp\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.cal_temp, cal_temp)
+
+    def test_get_mode(self, mode="pass-through"):
+        msg = 'set W_passthrough\r\nget W_mode\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, mode)
+
+    def test_wrong_commands(self):
+        msg = 'dummy;;\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, '')
+
+    def test_empty_commands(self):
+        msg = '\r\n'
+        for byte in msg:
+            self.assertTrue(self.system.parse(byte))
+        self.assertEqual(self.system.mode, '')
+
+
+if __name__ == '__main__':
+    unittest.main()
