f3b7860dc21ce6bd993dabaa5c92af808b94f4f0
Fabio Vitello <fvitello@users.noreply.github.com>
2023-01-16 11:33:56 +0100

    Update test_dbesm.py

--- tests/test_dbesm.py
+++ tests/test_dbesm.py
@@ -1,2 +1,12 @@
 import unittest
 from simulators.dbesm import System
+
+
+class TestDBESM(unittest.TestCase):
+    
+    def setUp(self):
+        self.system = System(system_type='DBESM')
+
+
+if __name__ == '__main__':
+    unittest.main()
