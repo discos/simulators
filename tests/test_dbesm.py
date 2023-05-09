fa50569d2cb37a8983302255b14c1fd7d73b106c
Fabio Vitello <fvitello@users.noreply.github.com>
2023-05-09 13:08:10 +0200

    Update test_dbesm.py
    sample test

--- tests/test_dbesm.py
+++ tests/test_dbesm.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from simulators import dbesm
 
 
@@ -6,6 +7,19 @@ class TestDBESM(unittest.TestCase):
 
     def setUp(self):
         self.system = dbesm.System()
+
+    def _send(self, message):
+        for byte in message[:-1]:
+            self.assertTrue(self.system.parse(byte))
+        response = self.system.parse(message[-1])
+        return response
+
+    def test_all_diag(self):
+        message = "DBE ALLDIAG\x0D"
+        response = self._send(message)
+        print(response)
+        # self.assertEqual(len(response), 1)
+        # self.assertEqual(response[0], 'ack')
 
 
 if __name__ == '__main__':
